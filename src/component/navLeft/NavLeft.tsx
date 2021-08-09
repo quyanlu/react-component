66cdb70ab67846f3b5f0b7b1706a42c284dfec56
quyanlu <quyanlu@meorient.com>
2021-08-09 19:54:38 +0800

    初始架构菜单栏配置

--- src/component/navLeft/NavLeft.tsx
+++ src/component/navLeft/NavLeft.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import {Menu} from 'antd';
 import {NavLink} from 'react-router-dom';
+import './NavLeft.scss';
+import {DropboxOutlined} from '@ant-design/icons';
+import MenuConfig from './../../config/menuConfig';
+import SubMenu from 'antd/es/menu/SubMenu';
 
 const NavLeft = (props: any) => {
+	const renderMenu = (data: any[]) => {
+		return data.map((item) => {
+			if (item.children) {
+				return (
+					<SubMenu title={item.title} key={item.key}>
+						{renderMenu(item.children)}
+					</SubMenu>
+				);
+			}
+			return <Menu.Item title={item.title} key={item.key}>
+				<NavLink to={item.key}>{item.title}</NavLink>
+			</Menu.Item>;
+		});
+	};
+
 	return (
-		<div>
+		<div className="nav-left">
 			<NavLink to="/home">
-				首页
+				<div className="logo">
+					<DropboxOutlined className="icon-style" />
+					<h1>Imooc MS</h1>
+				</div>
 			</NavLink>
-			<Menu>
-				{props.children}
+			<Menu theme="dark">
+				{renderMenu(MenuConfig)}
 			</Menu>
 		</div>
-	)
+	);
 };
 
 export default NavLeft;
